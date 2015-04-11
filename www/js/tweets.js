f4fa21ec05aa6f8f0f16a1dda236f670709641ae
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-04-11 18:43:19 -0400

    search form

--- www/js/tweets.js
+++ www/js/tweets.js
@@ -17,13 +17,19 @@ $(document).ready(function() {
 
     var SearchFormView = Backbone.View.extend({
         el: $("#search-form"),
+        events: {
+            'click #search' : 'search'
+        },
         initialize: function(){
             _.bindAll(this, 'render');
             this.render();
         },
         render: function(){
-            var template = _.template( $("#search-form-template").html());
-            this.$el.html( template(user) );
+            // var template = _.template( $("#search-form-template").html());
+            // this.$el.html( template(user) );
+        },
+        search: function() {
+            console.log('search');
         }
     });
     var searchForm = new SearchFormView();
